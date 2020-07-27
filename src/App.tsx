b48d541b23de1493c1623924a6ac9080adf2ab94
DaemonWalker <215136855@qq.com>
2020-07-27 20:19:22 +0800

    修改网站Header样式

--- src/App.tsx
+++ src/App.tsx
@@ -43,54 +43,57 @@ class App extends React.Component<IProps, IState> {
         return (
             <BrowserRouter>
                 <Layout className="App">
-                    <Navbar data={this.state.navData} />
-                    <Layout className="site-layout">
-                        <Header className="site-layout-background" style={{ padding: 0 }} >
-                            <Row style={{ display: 'inline-flex', width: "100%" }} justify="end">
-                                <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4} className="header-row">
-                                    <Search
-                                        placeholder="请输入你想搜索的内容"
-                                        enterButton={(
-                                            <Link to={`/search/${this.state.filter}`}><SearchOutlined /></Link>
-                                        )}
-                                        size="large"
-                                        style={{ display: 'inline-block', verticalAlign: 'middle' }}
-                                        onChange={(e) => this.setFilter(e)}
-                                    />
-                                </Col>
-                            </Row>
-                        </Header>
-                        <Content style={{ margin: '24px 16px 0' }}>
-                            <div className="site-layout-background app-container" style={{ padding: 24, minHeight: 360 }}>
-                                <Route path="/" exact render={(props) => (<Home {...props} key={new Date().getDate() + Math.random()}></Home>)} component={Home} />
-                                <Route path="/tags/:id?" render={(props) => (<Tags {...props} key={new Date().getDate() + Math.random()}></Tags>)} key="Tags" />
-                                <Route path="/article/:id?" render={(props) => (<Article {...props}></Article>)} />
-                                <Route path="/timeline/:id" component={ArticleTimeline} />
-                                <Route path="/search/:filter?" exact component={SearchResult} />
-                            </div>
-                        </Content>
-                        <Footer style={{ textAlign: 'center' }}>
-                            <p>
-                                <HyperLink href="https://github.com/DaemonWalker" text={(
-                                    <GithubOutlined style={{ fontSize: '3em' }} />
-                                )} target="_blank" />
-                            </p>
-                            <p>
-                                {`© ${new Date().getFullYear()} Daemon Walker`}
-                            </p>
-                            <p>
-                                Powered by
-              <HyperLink href="https://react.docschina.org/" text="React" target="_blank" />
-                                <HyperLink href="https://ant.design/index-cn" text="Ant Desgin(antd)" target="_blank" />
-                                <HyperLink href="https://dotnet.microsoft.com/" text=".Net Core" target="_blank" />
-                                <HyperLink href="https://www.centos.org/" text="CentOS" target="_blank" />
-                                <HyperLink href="https://www.docker.com/" text="Docker" target="_blank" />
-                            </p>
-                            <p>
-                                <HyperLink href="http://www.beian.gov.cn/portal/registerSystemInfo" text="辽ICP备16008708号-1" target="_blank" />
-                            </p>
-                        </Footer>
-                    </Layout>
+                    <Header className="site-layout-background header" style={{ padding: 0 }} >
+                        <Row>
+                            <Col xs={24} sm={14} md={14} lg={16} xl={18} xxl={20}>
+                                <Navbar data={this.state.navData} />
+                            </Col>
+                            <Col xs={0} sm={10} md={10} lg={8} xl={6} xxl={4} className="header-row">
+                                <Search
+                                    placeholder="请输入你想搜索的内容"
+                                    enterButton={(
+                                        <Link to={`/search/${this.state.filter}`}><SearchOutlined /></Link>
+                                    )}
+                                    size="large"
+                                    style={{ display: 'inline-block', verticalAlign: 'middle' }}
+                                    onChange={(e) => this.setFilter(e)}
+                                />
+                            </Col>
+                        </Row>
+                    </Header>
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div className="site-layout-background app-container" style={{ padding: 24, minHeight: 360 }}>
+                            <Route path="/" exact render={(props) => (<Home {...props} key={new Date().getDate() + Math.random()}></Home>)} component={Home} />
+                            <Route path="/tags/:id?" render={(props) => (<Tags {...props} key={new Date().getDate() + Math.random()}></Tags>)} key="Tags" />
+                            <Route path="/article/:id?" render={(props) => (<Article {...props}></Article>)} />
+                            <Route path="/timeline/:id" component={ArticleTimeline} />
+                            <Route path="/search/:filter?" exact component={SearchResult} />
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        <p>
+                            <HyperLink href="https://github.com/DaemonWalker" text={(
+                                <GithubOutlined style={{ fontSize: '3em' }} />
+                            )} target="_blank" />
+                        </p>
+                        <p>
+                            {`© ${new Date().getFullYear()} Daemon Walker`}
+                        </p>
+                        <p>
+                            Powered by
+              <a href="https://react.docschina.org/" target="_blank" >React</a>
+                            <a href="https://ant.design/index-cn" target="_blank" >Ant Desgin(antd)</a>
+                            <a href="https://dotnet.microsoft.com/" target="_blank">.Net Core</a>
+                            <a href="https://redis.io/" target="_blank">Redis</a>
+                            <a href="https://www.mongodb.com/" target="_blank">MongoDB</a>
+                            <a href="https://www.elastic.co/cn/" target="_blank">Elasticsearch</a>
+                            <a href="https://www.centos.org/" target="_blank">CentOS</a>
+                            <a href="https://www.docker.com/" target="_blank" >Docker</a>
+                        </p>
+                        <p>
+                            <HyperLink href="http://www.beian.gov.cn/portal/registerSystemInfo" text="辽ICP备16008708号-1" target="_blank" />
+                        </p>
+                    </Footer>
                 </Layout>
             </BrowserRouter>
         );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,11 +3,9 @@ import { Layout, Menu, Row, Typography } from 'antd'
 import 'antd/dist/antd.css'
 import React from 'react'
 import { NavDataModel } from '../models/navDataModel'
-import '../styles/navbar.css'
 import { HyperLink } from './hyperLink'
 
 const { SubMenu } = Menu;
-const { Sider } = Layout;
 const { Text, Title } = Typography;
 
 export class Navbar extends React.Component<IProps, IState> {
@@ -15,22 +13,7 @@ export class Navbar extends React.Component<IProps, IState> {
 
     render() {
         return (
-            <Sider collapsible
-                breakpoint="md"
-                collapsed={this.state && this.state.collapsed ? true : false}
-                onCollapse={this.onCollapse}
-                onBreakpoint={(broken: boolean) => {
-                    if (broken) {
-                        this.setState({
-                            collapsed: true
-                        })
-                    }
-                    else {
-                        this.setState({
-                            collapsed: false
-                        })
-                    }
-                }}>
+            <>
                 <div className="logo" >
                     <Row justify="space-around" align="middle">
                         <HyperLink href="/" text={(
@@ -41,36 +24,42 @@ export class Navbar extends React.Component<IProps, IState> {
                         )} />
                     </Row>
                 </div>
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu defaultSelectedKeys={['1']} mode="horizontal">
                     <SubMenu key="sub1" icon={<ClockCircleOutlined />} title="最新发表">
-                        {
-                            this.props.data.recents.map(e => (
-                                <Menu.Item key={new Date().getDate() + Math.random()}>
-                                    <HyperLink href={`/article/${e.url}`} text={e.content} />
-                                </Menu.Item>
-                            ))
-                        }
+                        <Menu.ItemGroup>
+                            {
+                                this.props.data.recents.map(e => (
+                                    <Menu.Item key={new Date().getDate() + Math.random()}>
+                                        <HyperLink href={`/article/${e.url}`} text={e.content} />
+                                    </Menu.Item>
+                                ))
+                            }
+                        </Menu.ItemGroup>
                     </SubMenu>
                     <SubMenu key="sub2" icon={<TagsOutlined />} title="标签">
-                        {
-                            this.props.data.tags.map(e => (
-                                <Menu.Item key={new Date().getDate() + Math.random()}>
-                                    <HyperLink href={`/tags/${e.url}`} text={e.content} />
-                                </Menu.Item>
-                            ))
-                        }
+                        <Menu.ItemGroup>
+                            {
+                                this.props.data.tags.map(e => (
+                                    <Menu.Item key={new Date().getDate() + Math.random()}>
+                                        <HyperLink href={`/tags/${e.url}`} text={e.content} />
+                                    </Menu.Item>
+                                ))
+                            }
+                        </Menu.ItemGroup>
                     </SubMenu>
                     <SubMenu key="sub3" icon={<CalendarOutlined />} title="时间">
-                        {
-                            this.props.data.timeLine.map(e => (
-                                <Menu.Item key={new Date().getDate() + Math.random()}>
-                                    <HyperLink href={`/timeline/${e.url}`} text={e.content} />
-                                </Menu.Item>
-                            ))
-                        }
+                        <Menu.ItemGroup>
+                            {
+                                this.props.data.timeLine.map(e => (
+                                    <Menu.Item key={new Date().getDate() + Math.random()}>
+                                        <HyperLink href={`/timeline/${e.url}`} text={e.content} />
+                                    </Menu.Item>
+                                ))
+                            }
+                        </Menu.ItemGroup>
                     </SubMenu>
                 </Menu>
-            </Sider >
+            </>
         );
     }
 
